1600b0d90403903eadebbdb89373b8845610e33c
Goran Stevanovic <stevanovicgoran@proton.me>
2023-03-22 20:11:54 +0100

    Add failing tests

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -3,9 +3,22 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('Header', () => {
-  it('displays the Header text', () => {
+  it('displays the app title', () => {
     render(<Header />);
 
-    expect(screen.getByText(/Header/)).toBeInTheDocument();
+    expect(screen.getByText(/Color Scheme Lab/)).toBeInTheDocument();
+  });
+
+  it('displays the navigation bar with all the links', () => {
+    render(<Header />);
+
+    expect(screen.getByText(/Create/)).toBeInTheDocument();
+    expect(screen.getByText(/Saved/)).toBeInTheDocument();
+  });
+
+  it('displays the select menu with label', () => {
+    render(<Header />);
+
+    expect(screen.getByLabelText(/Color mode/)).toBeInTheDocument();
   });
 });
