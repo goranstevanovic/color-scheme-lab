f37c8f3d167f7c5ee0536c3f32c20ea87470fbdb
Goran Stevanovic <stevanovicgoran@proton.me>
2023-03-23 16:59:19 +0100

    Create component tests

--- src/components/ColorHarmonyOptions.test.tsx
+++ src/components/ColorHarmonyOptions.test.tsx
@@ -3,9 +3,27 @@ import { render, screen } from '@testing-library/react';
 import ColorHarmonyOptions from './ColorHarmonyOptions';
 
 describe('ColorHarmonyOptions', () => {
-  it('displays the ColorHarmonyOptions test', () => {
+  it('displays the base color input', () => {
     render(<ColorHarmonyOptions />);
 
-    expect(screen.getByText(/ColorHarmonyOptions/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Base color/)).toBeInTheDocument();
+  });
+
+  it('displays the color harmony rule select menu', () => {
+    render(<ColorHarmonyOptions />);
+
+    expect(screen.getByLabelText(/Color harmony rule/)).toBeInTheDocument();
+  });
+
+  it('displays the color harmony rule description', () => {
+    render(<ColorHarmonyOptions />);
+
+    expect(screen.getByText(/This color scheme/)).toBeInTheDocument();
+  });
+
+  it('displays the create color scheme button', () => {
+    render(<ColorHarmonyOptions />);
+
+    expect(screen.getByText(/Create Color Scheme/)).toBeInTheDocument();
   });
 });
