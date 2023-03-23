05a60f0220261b9050a6bbdc83a04ad23a4d66ec
Goran Stevanovic <stevanovicgoran@proton.me>
2023-03-23 17:08:37 +0100

    Create grid and position elements

--- src/components/ColorHarmonyOptions.tsx
+++ src/components/ColorHarmonyOptions.tsx
@@ -1,11 +1,13 @@
+import styles from './ColorHarmonyOptions.module.css';
+
 function ColorHarmonyOptions() {
   return (
-    <section>
-      <div>
+    <section className={styles.container}>
+      <div className={styles.baseColor}>
         <label htmlFor="base-color">Base color</label>
         <input id="base-color" type="color" value="#2196F3" />
       </div>
-      <div>
+      <div className={styles.colorHarmonyRule}>
         <label htmlFor="color-harmony-rule">Color harmony rule</label>
         <select id="color-harmony-rule">
           <option value="analogic">Analogous</option>
@@ -16,15 +18,13 @@ function ColorHarmonyOptions() {
           <option value="quad">Square</option>
         </select>
       </div>
-      <div>
-        <p>
-          This color scheme creates a sense of harmony and unity, as the colors
-          blend together seamlessly. It can convey a feeling of warmth and
-          comfort. This scheme is best used when you want to create a peaceful,
-          relaxing mood, such as in a bedroom or spa.
-        </p>
-      </div>
-      <button>Create Color Scheme</button>
+      <p className={styles.description}>
+        This color scheme creates a sense of harmony and unity, as the colors
+        blend together seamlessly. It can convey a feeling of warmth and
+        comfort. This scheme is best used when you want to create a peaceful,
+        relaxing mood, such as in a bedroom or spa.
+      </p>
+      <button className={styles.button}>Create Color Scheme</button>
     </section>
   );
 }
