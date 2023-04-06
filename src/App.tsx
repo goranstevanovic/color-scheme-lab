cc1f2af0ad245b50b5b26b146ba1ce6f5aa41951
Goran Stevanovic <stevanovicgoran@proton.me>
2023-04-06 16:57:42 +0200

    Create ColorScheme component and import it into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import ColorHarmonyOptions from './components/ColorHarmonyOptions';
+import ColorScheme from './components/ColorScheme';
 
 function App() {
   return (
     <>
       <Header />
       <ColorHarmonyOptions />
+      <ColorScheme />
       <p>App</p>
     </>
   );
---
+++ src/components/ColorScheme/index.tsx
@@ -0,0 +1,5 @@
+function ColorScheme() {
+  return <p>ColorScheme</p>;
+}
+
+export default ColorScheme;
