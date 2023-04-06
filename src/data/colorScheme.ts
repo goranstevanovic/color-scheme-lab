f3c389972276ede359255154522d1e9f90f97081
Goran Stevanovic <stevanovicgoran@proton.me>
2023-04-06 17:11:34 +0200

    Add color scheme data type declaration

--- src/data/colorScheme.ts
+++ src/data/colorScheme.ts
@@ -1,3 +1,146 @@
+export type ColorScheme = {
+  mode: string;
+  count: string;
+  colors: Seed[];
+  seed: Seed;
+  image: Image;
+  _links: ColorSchemeLinks;
+  _embedded: Embedded;
+};
+
+export type Embedded = {};
+
+export type ColorSchemeLinks = {
+  self: string;
+  schemes: Schemes;
+};
+
+export type Schemes = {
+  monochrome: string;
+  'monochrome-dark': string;
+  'monochrome-light': string;
+  analogic: string;
+  complement: string;
+  'analogic-complement': string;
+  triad: string;
+  quad: string;
+};
+
+export type Seed = {
+  hex: Hex;
+  rgb: Rgb;
+  hsl: Hsl;
+  hsv: Hsv;
+  name: Name;
+  cmyk: Cmyk;
+  XYZ: Xyz;
+  image: Image;
+  contrast: Contrast;
+  _links: SeedLinks;
+  _embedded: Embedded;
+};
+
+export type Xyz = {
+  fraction: XYZFraction;
+  value: string;
+  X: number;
+  Y: number;
+  Z: number;
+};
+
+export type XYZFraction = {
+  X: number;
+  Y: number;
+  Z: number;
+};
+
+export type SeedLinks = {
+  self: Self;
+};
+
+export type Self = {
+  href: string;
+};
+
+export type Cmyk = {
+  fraction: CmykFraction;
+  value: string;
+  c: number;
+  m: number;
+  y: number;
+  k: number;
+};
+
+export type CmykFraction = {
+  c: number;
+  m: number;
+  y: number;
+  k: number;
+};
+
+export type Contrast = {
+  value: string;
+};
+
+export type Hex = {
+  value: string;
+  clean: string;
+};
+
+export type Hsl = {
+  fraction: HslFraction;
+  h: number;
+  s: number;
+  l: number;
+  value: string;
+};
+
+export type HslFraction = {
+  h: number;
+  s: number;
+  l: number;
+};
+
+export type Hsv = {
+  fraction: HsvFraction;
+  value: string;
+  h: number;
+  s: number;
+  v: number;
+};
+
+export type HsvFraction = {
+  h: number;
+  s: number;
+  v: number;
+};
+
+export type Image = {
+  bare: string;
+  named: string;
+};
+
+export type Name = {
+  value: string;
+  closest_named_hex: string;
+  exact_match_name: boolean;
+  distance: number;
+};
+
+export type Rgb = {
+  fraction: RgbFraction;
+  r: number;
+  g: number;
+  b: number;
+  value: string;
+};
+
+export type RgbFraction = {
+  r: number;
+  g: number;
+  b: number;
+};
+
 const colorSchemeSample = {
   mode: 'monochrome',
   count: '4',
