a79e30e89753ffae879898b84a6cfe2b2bb27b5f
Goran Stevanovic <stevanovicgoran@proton.me>
2023-04-08 12:33:08 +0200

    Show Create page on index route

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,15 @@ import {
 } from 'react-router-dom';
 
 import Root from './pages/Root';
+import Create from './pages/Create';
 
 function App() {
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route path="/" element={<Root />} />)
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Create />} />
+      </Route>
+    )
   );
 
   return (
