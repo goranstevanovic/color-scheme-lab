43bd163e5ec29f69f5c72110eee93669748d8684
Goran Stevanovic <stevanovicgoran@proton.me>
2023-04-11 16:16:52 +0200

    Fix number of colors that are displayed for each color harmony rule
    For some color schemes, fetched color scheme data object contains
    `count` field with the correct number, but the `colors` array
    contains more colors than necessary. `colorQty` is used to hold
    the actual number of colors needed for current color scheme, and
    it's used to slice the `colors` array to correct length.

--- src/components/ColorScheme/index.tsx
+++ src/components/ColorScheme/index.tsx
@@ -14,6 +14,13 @@ function ColorScheme({
   colorSchemeData,
   colorSchemeTitle,
 }: colorSchemeProps) {
+  // For some color schemes, fetched color scheme data object contains `count`
+  // field with correct number, but the `colors` array contains more colors
+  // than necessary. `colorQty` is used to hold the actual number of colors
+  // needed for current color scheme, and it's used to slice the `colors` array
+  // to correct length
+  const colorQty = Number(colorSchemeData.count);
+
   const button = <Button type={buttonType} onClick={() => {}} />;
   const baseColorSwatch = (
     <ColorSwatch
@@ -22,13 +29,15 @@ function ColorScheme({
       textColor={colorSchemeData.seed.contrast.value}
     />
   );
-  const remainingColorSwatches = colorSchemeData.colors.map((color) => (
-    <ColorSwatch
-      colorCode={color.hex.value}
-      colorName={color.name.value}
-      textColor={color.contrast.value}
-    />
-  ));
+  const remainingColorSwatches = colorSchemeData.colors
+    .slice(0, colorQty)
+    .map((color) => (
+      <ColorSwatch
+        colorCode={color.hex.value}
+        colorName={color.name.value}
+        textColor={color.contrast.value}
+      />
+    ));
 
   return (
     <div className={styles.colorSchemeWrapper}>
