3c9cc1d7f38652978932b2c7e0982ebdcdba2ef2
Goran Stevanovic <stevanovicgoran@proton.me>
2023-04-17 14:21:21 +0200

    Test if text is displayed with no saved color schemes

--- src/pages/Saved.test.tsx
+++ src/pages/Saved.test.tsx
@@ -1,11 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Saved from './Saved';
 
 describe('Saved', () => {
-  it('should display the sample text', () => {
-    render(<Saved />);
+  it('displays text when no color schemes are saved', () => {
+    render(<Saved />, { wrapper: BrowserRouter });
 
-    expect(screen.getByText(/Saved page/)).toBeInTheDocument();
+    expect(
+      screen.getByText('There are no saved color schemes.')
+    ).toBeInTheDocument();
   });
 });
