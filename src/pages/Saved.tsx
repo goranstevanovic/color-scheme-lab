2b1709b0058cd1f3622fde8233fe2538db297f35
Goran Stevanovic <stevanovicgoran@proton.me>
2023-04-17 13:37:48 +0200

    Show saved color schemes (color swatches and Delete button)

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react';
+import ColorScheme from '../components/ColorScheme';
+
+type SavedColorSchemeType = {
+  [key: string]: any;
+};
 
 function Saved() {
-  const [savedColorSchemes, setSavedColorSchemes] = useState(null);
+  const [savedColorSchemes, setSavedColorSchemes] =
+    useState<SavedColorSchemeType>({});
 
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem('savedColorSchemes') || '{}');
@@ -10,12 +16,22 @@ function Saved() {
     }
   }, []);
 
+  let output = [];
+  if (Object.keys(savedColorSchemes).length > 0) {
+    for (const colorSchemeId in savedColorSchemes) {
+      output.push(
+        <ColorScheme
+          buttonType="delete"
+          colorSchemeData={savedColorSchemes[colorSchemeId]}
+        />
+      );
+    }
+  }
+
   return (
     <>
       <p>Saved page</p>
-      {savedColorSchemes
-        ? 'There are saved color schemes'
-        : 'There are no saved color schemes'}
+      {output}
     </>
   );
 }
