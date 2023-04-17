85342c02f049db4151f168b5988af7c574847f25
Goran Stevanovic <stevanovicgoran@proton.me>
2023-04-17 13:11:36 +0200

    Load saved color schemes from local storage into state

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -1,5 +1,23 @@
+import { useState, useEffect } from 'react';
+
 function Saved() {
-  return <p>Saved page</p>;
+  const [savedColorSchemes, setSavedColorSchemes] = useState(null);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('savedColorSchemes') || '{}');
+    if (Object.keys(items).length !== 0) {
+      setSavedColorSchemes(items);
+    }
+  }, []);
+
+  return (
+    <>
+      <p>Saved page</p>
+      {savedColorSchemes
+        ? 'There are saved color schemes'
+        : 'There are no saved color schemes'}
+    </>
+  );
 }
 
 export default Saved;
