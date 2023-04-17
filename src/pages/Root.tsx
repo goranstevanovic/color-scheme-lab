e40d0669cfe8e5489699b9733e2400f18fc2f0f1
Goran Stevanovic <stevanovicgoran@proton.me>
2023-04-17 14:43:26 +0200

    Add spacing between saved color schemes

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
 import Header from '../components/Header';
+import styles from './Root.module.css';
 
 function Root() {
   return (
     <>
       <Header />
-      <main>
+      <main className={styles.main}>
         <Outlet />
       </main>
     </>
